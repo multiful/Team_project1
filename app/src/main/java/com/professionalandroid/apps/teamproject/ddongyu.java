cd1cdde54497fcb51cea62f15eb6e2d3cf5e744f
multiful <rlaehdrb2485@naver.com>
2023-05-31 03:37:02 +0900

    hi

---
+++ app/src/main/java/com/professionalandroid/apps/teamproject/ddongyu.java
@@ -0,0 +1,14 @@
+package com.professionalandroid.apps.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ddongyu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ddongyu);
+    }
+}
